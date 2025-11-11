84c02bc3a0c7286df0ed7e6e9fa5ae05c37d03b3
Woong1016 <yol7122@naver.com>
2025-11-12 00:00:13 +0900

    [Feat] 브루트 함수 쉬운버전 완료
    어려운버전은 시험기간을 위해 미리 공부하고 풀어보기

--- GA_Project_2023137067/Assets/Scripts/2025_11_05/BruteForceSample.cs
+++ GA_Project_2023137067/Assets/Scripts/2025_11_05/BruteForceSample.cs
@@ -6,7 +6,7 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
- 
+
 public class BruteForceSample : MonoBehaviour
 {
 
@@ -14,7 +14,7 @@ public class BruteForceSample : MonoBehaviour
     string secretpin;
     Coroutine runningRoutine;
 
-    
+
 
     // Start is called before the first frame update
     void Start()
@@ -31,7 +31,7 @@ public class BruteForceSample : MonoBehaviour
             return;
 
         }
-        runningRoutine = StartCoroutine(bruteForceRoutine());
+        runningRoutine = StartCoroutine(bruteForceRoutineTest());
     }
 
     IEnumerator bruteForceRoutine()
@@ -50,7 +50,7 @@ public class BruteForceSample : MonoBehaviour
         int multShot = 5;
         int tripleShot = 7;
 
-        for(int i = 0; i < max; i++)
+        for (int i = 0; i < max; i++)
         {
 
         }
@@ -63,7 +63,7 @@ public class BruteForceSample : MonoBehaviour
             string tryString = i.ToString("D4");
             tryCount++;
 
-            if(tryString == secretpin)
+            if (tryString == secretpin)
             {
                 sw.Stop();
                 double seconds = sw.Elapsed.TotalSeconds;
@@ -72,9 +72,9 @@ public class BruteForceSample : MonoBehaviour
                 yield break;
             }
 
-            if(i%100 == 0)
+            if (i % 100 == 0)
             {
-                yield return null; 
+                yield return null;
             }
         }
         sw.Stop();
@@ -83,4 +83,73 @@ public class BruteForceSample : MonoBehaviour
 
     }
     
+    IEnumerator bruteForceRoutineTest()
+    {
+        // 정신 나갈 것 같다
+
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+
+       
+        int totalCostLimit = 15;
+        int quickShotMaxCount = 2, quickShotDamage = 6, quickShotCost = 2;
+        int heavyShotMaxCount = 2, heavyShotDamage = 8, heavyShotCost = 3;
+        int multiShotMaxCount = 1, multiShotDamage = 16, multiShotCost = 5;
+        int tripleShotMaxCount = 1, tripleShotDamage = 24, tripleShotCost = 7;
+
+      
+        (int q, int h, int m, int t, int cost, int dmg) best;
+        best = (q: 0, h: 0, m: 0, t: 0, cost: 0, dmg: -1);
+
+        
+
+        int totalCombinations = 0;
+
+        
+        for (int q = 0; q <= quickShotMaxCount; q++)
+        {
+            for (int h = 0; h <= heavyShotMaxCount; h++)
+            {
+                for (int m = 0; m <= multiShotMaxCount; m++)
+                {
+                    for (int t = 0; t <= tripleShotMaxCount; t++)
+                    {
+                        totalCombinations++;
+
+                        
+                        int currentCost = (q * quickShotCost) + (h * heavyShotCost) + (m * multiShotCost) + (t * tripleShotCost);
+                        int currentDamage = (q * quickShotDamage) + (h * heavyShotDamage) + (m * multiShotDamage) + (t * tripleShotDamage);
+
+                       
+                        if (currentCost <= totalCostLimit)
+                        {
+                            
+                            if (currentDamage > best.dmg)
+                            {
+                               
+                                best = (q, h, m, t, currentCost, currentDamage);
+                            }
+                        }
+                    }
+                }
+            }
+
+
+
+
+        }
+
+        
+        sw.Stop();
+        double seconds = sw.Elapsed.TotalSeconds;
+
+      
+        UnityEngine.Debug.Log($"총 {totalCombinations}개 조합 | 소요 시간 = {seconds:F6}초");
+        UnityEngine.Debug.Log($" 퀵샷({best.q}) 헤비샷({best.h}) 멀티샷({best.m}) 트리플샷({best.t}), 코스트 {best.cost}/{totalCostLimit}, 데미지({best.dmg})");
+
+
+        runningRoutine = null;
+        yield break;
+    }
+
 }
